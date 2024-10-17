0fa936d7a245ad0c26dcc68282c3d4b6e62e9ab5
Laura Comar <lautmcomar@gmail.com>
2024-10-17 13:59:00 -0400

    Handle stock availability and disable purchase button when out of stock

--- main.js
+++ main.js
@@ -5,5 +5,10 @@ document.addEventListener("DOMContentLoaded", () => {
     selectSize.addEventListener("change", () => { // event listener add to size dropdown
         const sizedSelected = selectSize.options[selectSize.selectedIndex]; // get the selected size 
         const price = sizedSelected.getAttribute("data-price"); // get price according to selected size 
-    })
-})
+        const status = sizedSelected.getAttribute("data-stock"); // get status according to size selected
+
+        productPrice.textContent = `$${price}`; // update price according to size selected
+
+        buyButton.disable = (status === "Not in stock") // disable buy button if size is not in stock
+    });
+});

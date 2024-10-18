bbb251aba25d6e768e15c4afba6ad34cd6004426
Laura Comar <lautmcomar@gmail.com>
2024-10-18 10:30:46 -0400

    Implement event delegation to handle dynamically added products (first attempt to do task 5)

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const selectSize = document.getElementById("size-selector"); // get the select size by id
     const productPrice = document.getElementById("product-price"); // get the prod price by id
-    const buyButton = document.getElementById("buy-price"); // get buy button by id
+    const buyButton = document.getElementById("buy-button"); // get buy button by id
     selectSize.addEventListener("change", () => { // event listener add to size dropdown
         const sizeSelected = selectSize.options[selectSize.selectedIndex]; // get the selected size 
         const price = sizeSelected.getAttribute("data-price"); // get price according to selected size 
@@ -11,15 +11,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
         productPrice.textContent = `$${price}`; // update price according to size selected
 
-        buyButton.disable = (status === "Not in stock") // disable buy button if size is not in stock
+        buyButton.disabled = (status === "Out of stock") // disable buy button if size is not in stock
+    });
+
+
+    //Task 4
+    buyButton.addEventListener("click", () => { // event listener to buy button
+        const sizeSelected = selectSize.options[selectSize.selectedIndex]; //get size selected
+        const status = sizeSelected.getAttribute("data-stock"); //get stock status
+        if (status === "in-stock") { // check if size is available
+            alert("Thank you for your purchase"); // use alert to show confirmation 
+        } else { alert("Size is not available") } // message that product cant be purchased bc is out of stock
     });
 });
 
-//Task 4
-buyButton.addEventListener("click", () => { // event listener to buy button
-    const sizeSelected = selectSize.options[selectSize.selectedIndex]; //get size selected
-    const status = sizeSelected.getAttribute("data-stock"); //get stock status
-    if (status === "in-stock") { // check if size is available
-        alert("Thank you for your purchase"); // use alert to show confirmation 
-    } else { alert("Size is not available") } // message that product cant be purchased bc is out of stock
+//Task 5: OPTIONAL
+const addProduct = document.getElementById("add-product"); // get add product from ID
+addProduct.addEventListener("submit", () => {
+    preventDefault();
+    const newProduct = document.getElementById("product-name").value; //get product name
+    const newProductPrice = document.getElementById("product-price").value; //get product price
+    const newProductStatus = document.getElementById("new-product-availability").value; //get product availability status
+
 });
